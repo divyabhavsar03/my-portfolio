bb61586bab9872734e6ff9835a9fb7d50dc8c587
divyabhavsar03 <bhavsardivya59@gmail.com>
2025-07-07 22:22:22 -0600

    Updated Hero Section

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -7,7 +7,7 @@ const Hero = () => {
   return (
     <div id='home' className='hero'>
       <img src={profile_img} alt="" />
-      <h1>Hi, <span className='hero-name-span'>I'm Divya Bhavsar</span> from Calgary, AB, Canada. <br />I build <span className='hero-website-span'>websites</span> 💻 and <span className='hero-geomatics-span'>geospatial experiences</span>🛰️.</h1>
+      <h1>Hi, <span className='hero-name-span'>I'm Divya Bhavsar</span> from Calgary, AB, Canada. <br />I build <span className='hero-website-span'>websites</span> 💻 and <span className='hero-geomatics-span'>geospatial experiences </span>🛰️.</h1>
       <center>
       <p>I am a <span className='hero-geo-deg-span'>Geomatics Engineering MEng student</span> at the <span className='hero-uni-span'>University of Calgary</span> with a strong background in <span className='hero-comp-deg-span'>Computer Engineering</span>. I specialize in <span className='hero-skill-span'>GNSS Satellite Positioning, Photogrammetry, GIS using ArcGIS Pro, and Web Development</span>.
       <br />
