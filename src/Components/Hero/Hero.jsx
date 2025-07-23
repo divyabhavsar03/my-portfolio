a31ecc51eccaab826f0e1cdf8c15082f6fc7674d
divyabhavsar03 <bhavsardivya59@gmail.com>
2025-07-22 18:04:41 -0600

    Updated Hero Section

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -15,8 +15,8 @@ const Hero = () => {
       </p>
       </center>
       <div className="hero-action">
-        <div className="hero-connect"><AnchorLink className='anchor-link' offset={50} href='#contact'>Connect with me</AnchorLink></div>
-        <div className="hero-resume"><a href="/Divya-Bhavsar-Resume.pdf" download target="_blank" rel="noopener noreferrer" className='anchor-link'>My resume</a></div>
+        <div className="hero-connect"><a href="/Divya-Bhavsar-WebDev-Resume.pdf" download target="_blank" rel="noopener noreferrer" className='anchor-link'>Web Dev Resume</a></div>
+        <div className="hero-resume"><a href="/Divya-Bhavsar-Resume.pdf" download target="_blank" rel="noopener noreferrer" className='anchor-link'>GNSS resume</a></div>
       </div>
     </div>
   )
