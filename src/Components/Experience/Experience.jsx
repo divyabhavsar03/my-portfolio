ef7e7cd57a590741b5b5faae50cfeebec01a3e07
divyabhavsar03 <bhavsardivya59@gmail.com>
2025-10-26 00:38:25 -0600

    Updating Education & CSS Section

--- src/Components/Experience/Experience.jsx
+++ src/Components/Experience/Experience.jsx
@@ -19,12 +19,16 @@ const Experience = () => {
                             <h4>Assisting Dr. Hongzhou Yang in the course by supporting lab sessions, helping students with questions, grading, and encouraging academic learning.</h4>
                         </div>
                     </div>
-                    <div className="experience-gtu">
-                        <a href="https://gtu.ac.in/" target="_blank" rel="noopener noreferrer"><img src={GTULogo} alt="GTU-logo" /></a>
-                        <div className="gtu-details">
-                            <h2>Bachelors in Computer Engineering (2019 - 2023)</h2>
-                            <h3>Gujarat Technological University, Ahmedabad, India <img src="https://upload.wikimedia.org/wikipedia/en/4/41/Flag_of_India.svg" alt="flag-of-india" style={{ width: '24px', height: 'auto', marginLeft: '4px', alignItems: 'center', filter:'none' }} /></h3>
-                            <h4>CGPA: 8.95/10 CGPA</h4>
+                    <div className="experience-linestar">
+                        <a href="https://gtu.ac.in/" target="_blank" rel="noopener noreferrer"><img src={GTULogo} alt="linestar-logo" /></a>
+                        <div className="linestar-details">
+                            <h2>Shopify Content Assistant</h2>
+                            <h3>LineStar Utility Supply, Calgary, Canada <img src="https://flagcdn.com/ca.svg" style={{ width: '24px', height: 'auto', marginLeft: '4px', alignItems: 'center', filter:'none' }} alt='flag-of-canada' /></h3>
+                            <h4>Assisting in setting up and managing the Shopify online store.
+Performing product entry, descriptions, images, variants, and basic SEO optimization.
+Conducting website review and suggested improvements for usability and content.
+Gaining hands-on experience in office workflow, time, and project management.
+Collaborating with the team, improving communication and professional skills.</h4>
                         </div>
                     </div>
                 </div>
