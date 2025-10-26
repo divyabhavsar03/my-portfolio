29c3d5f6cffe8f4e27a0cd757d577d93c94fbdba
divyabhavsar03 <bhavsardivya59@gmail.com>
2025-10-26 00:47:51 -0600

    Updating Experience & CSS Section

--- src/Components/Experience/Experience.jsx
+++ src/Components/Experience/Experience.jsx
@@ -13,7 +13,7 @@ const Experience = () => {
                     <div className="experience-uofc">
                         <a href="https://ucalgary.ca/" target="_blank" rel="noopener noreferrer"><img src={uofcLogo} alt="UCalgary Logo" /></a>
                         <div className="uofc-details">
-                            <h2>Teaching Assistant</h2>
+                            <h2>Teaching Assistant (September 2025 - Present)</h2>
                             <h3>University of Calgary, Calgary, Canada <img src="https://flagcdn.com/ca.svg" style={{ width: '24px', height: 'auto', marginLeft: '4px', alignItems: 'center', filter:'none' }} alt='flag-of-canada' /></h3>
                             <h4>ENGG 680: Introduction to Digital Engineering</h4>
                             <h4>Assisting Dr. Hongzhou Yang in the course by supporting lab sessions, helping students with questions, grading, and encouraging academic learning.</h4>
@@ -22,7 +22,7 @@ const Experience = () => {
                     <div className="experience-linestar">
                         <img src={linestarLogo} alt="linestar-logo" />
                         <div className="linestar-details">
-                            <h2>Shopify Content Assistant</h2>
+                            <h2>Shopify Content Assistant (August 2025 - Present)</h2>
                             <h3>LineStar Utility Supply, Calgary, Canada <img src="https://flagcdn.com/ca.svg" style={{ width: '24px', height: 'auto', marginLeft: '4px', alignItems: 'center', filter:'none' }} alt='flag-of-canada' /></h3>
                             <h4>Assisting in setting up and managing the Shopify online store.
 Performing product entry, descriptions, images, variants, and basic SEO optimization.
